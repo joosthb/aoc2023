37f1e6867dbcfac24c3959fc87da9772a784273f
joosthb <joost@hendricksen.net>
2023-12-01 17:04:26 +0100

    linting

--- 1.py
+++ 1.py
@@ -1,12 +1,13 @@
+"""Module providing a function parsing regex."""
 import re
 
-with open('1.txt', 'r') as fp:
-  lines = fp.readlines()
+with open('1.txt', 'r', encoding="utf-8") as fp:
+    lines = fp.readlines()
 
 pattern = r'[^0-9]'
+total = 0
 
-sum = 0
 for line in lines:
-  digits = re.sub(pattern, '', line)
-  sum += int(digits[:1])*10 + int(digits[-1:])
-print(sum)
+    digits = re.sub(pattern, '', line)
+    total += int(digits[:1])*10 + int(digits[-1:])
+print(total)
--- 2.py
+++ 2.py
@@ -1,16 +1,16 @@
 import re
 
 transtable = {'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
+pattern = r'[^0-9]'
+sum = 0
 
 with open('1.txt', 'r') as fp:
-  lines = fp.readlines()
+    lines = fp.readlines()
 
-pattern = r'[^0-9]'
-
-sum = 0
 for line in lines:
-  for number in transtable:
-    line = line.replace(number, number + transtable[number] + number )
-  digits = re.sub(pattern, '', line)
-  sum += int(digits[:1])*10 + int(digits[-1:])
+    for number in transtable.items():
+        line = line.replace(number[0], number[0] + number[1] + number[0] )
+    digits = re.sub(pattern, '', line)
+    sum += int(digits[:1])*10 + int(digits[-1:])
+
 print(sum)
