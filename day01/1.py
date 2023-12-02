8e84d0f4cb5b9c62c4494a9f8407a0952f88c8f6
joosthb <joost@hendricksen.net>
2023-12-02 14:14:56 +0100

    day 2 challenge 1

--- day01/1.py
+++ day01/1.py
@@ -3,10 +3,9 @@ import re
 with open('1.txt', 'r', encoding="utf-8") as fp:
     lines = fp.readlines()
 
-pattern = r'[^0-9]'
 total = 0
 
 for line in lines:
-    digits = re.sub(pattern, '', line)
+    digits = re.sub(r'[^0-9]', '', line)
     total += int(digits[:1])*10 + int(digits[-1:])
 print(total)
--- day02/1.py
+++ day02/1.py
@@ -1,6 +1,22 @@
 import re
 
+def valid_game(takes: str) -> bool:
+    ''' validates game '''
+    maxcubes = {'red': 12, 'green': 13, 'blue': 14}
+    for take in takes.split('; '):
+        for colorcount in take.split(', '):
+            count, color = colorcount.split(' ')
+            if int(count) > maxcubes[color]:
+                return False
+    return True
+
 with open('input', 'r', encoding="utf-8") as fp:
-    inputlines = fp.readlines()
+    games = fp.read().splitlines()
 
+total = 0
 
+for game in games:
+    gameid, takes = game.split(': ')
+    if valid_game(takes):
+        total += int(re.sub(r'[^0-9]', '', gameid))
+print(total)
