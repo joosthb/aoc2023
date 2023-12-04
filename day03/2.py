c896a8b39521aa1862476550f18c4a13918ea857
joosthb <joost@hendricksen.net>
2023-12-04 22:58:53 +0100

    you're so ugly

--- day03/2.py
+++ day03/2.py
@@ -1,38 +1,39 @@
 import re
 
-def ispartnumber(environment: list) -> bool:
-    for inchar in environment:
-        if inchar != '.' and not inchar.isdigit():
-            return True
-    return False
-
-with open('testinput', 'r', encoding="utf-8") as fp:
+with open('input', 'r', encoding="utf-8") as fp:
     lines = fp.read().splitlines()
 
-repatern = re.compile(r'\*')
+repatern = re.compile(r'\d+')
 
 total = 0
+field = {}
 
 for y, line in enumerate(lines):
     matches = repatern.finditer(line)
     for match in matches:
-        for yy in range(y-1, y+2):
-            for xx in range(match.span()[0]-1, match.span()[0]+2):
-                print(lines[yy][xx])
-        # print(y, match.span()[0])
-        # print(lines[y][match.span()[0]])
+        surroundings = []
 
-    
-        # start = max(match.start()-1, 0)
-        # end = min(match.end()+1, len(line))
+        start = max(match.start()-1, 0)
+        end = min(match.end()+1, len(line))
 
-        # adjacent = []
-        # if y:
-        #     adjacent += lines[y-1][start:end]
-        # adjacent += line[start:end]
-        # if y + 1 < len(lines):
-        #     adjacent += lines[y+1][start:end]
-        # if ispartnumber(adjacent):
-        #     total += int(match.group())
+        if y:
+            surroundings += [(y-1, x) for x in range(start,end)]
+        if start:
+            surroundings += [(y, start)]
+        if end != len(line):
+            surroundings += [(y, end-1)]
+        if y + 1 < len(lines):
+            surroundings += [(y+1, x) for x in range(start, end)]
+
+        for cord in surroundings:
+            if lines[cord[0]][cord[1]] == '*':
+                if cord in field:
+                    field[cord] += [int(match.group())]
+                else: 
+                    field[cord] = [int(match.group())]
+
+for cord, value in field.items():
+    if len(value) == 2:
+        total += value[0] * value[1]
 
 print(total)
