0412a4c5882c49301ac37b405f960bed88b433a6
joosthb <joost@hendricksen.net>
2023-12-05 15:04:44 +0100

    ex 1

--- day05/1.py
+++ day05/1.py
@@ -8,15 +8,14 @@ class Almanac:
             if ':' in line:
                 if 'map' in line:
                     currentchapter = line.split(' map:')[0]
-                    self._map[currentchapter] = {}
+                    self._map[currentchapter] = []
                 # seeds
                 else:
                     self._seeds = [int(seed) for seed in line.split(': ')[1].split(' ')]
             else:
                 if line != '':
-                    dest, source, length = line.split(' ')
-                    for x in range(int(length)):
-                        self._map[currentchapter][int(source)+x] = int(dest)+x
+                    # dest, source, length
+                    self._map[currentchapter] += [[int(x) for x in line.split(' ')]]
     @property
     def seeds(self) -> list[int]:
         return self._seeds
@@ -24,28 +23,27 @@ class Almanac:
     @property
     def map(self) -> dict:
         return self._map
+    
+    def _sourcetodest(self, chapter: str, sourceid: int) -> int:
+        '''
+        Function to map source id to destination id using almanac chapter map
+        '''
+        for map in self._map[chapter]:
+            if map[1] < sourceid < (map[1] + map[2]):
+                return sourceid - map[1] + map[0]
+        return sourceid
+    
+    def seedtolocation(self, seedid: int) -> int:
+        input = seedid
+        for fromto in ['seed-to-soil', 'soil-to-fertilizer', 'fertilizer-to-water','water-to-light', 'light-to-temperature', 'temperature-to-humidity', 'humidity-to-location']:
+            output = self._sourcetodest(fromto, input)
+            input = output
+        return output
 
-def mapfunc(almamap, table, id) -> int:
-    if id in almamap[table]:
-        return almamap[table][id]
-    else:
-        return id
 
 if __name__ == "__main__":
-    with open('testinput', 'r', encoding="utf-8") as fp:
+    with open('input', 'r', encoding="utf-8") as fp:
         lines = fp.read().splitlines()
     almanac = Almanac(lines)
-    print(almanac.map)
-    print(almanac.seeds)
-
-    # # closest = None
-    # for seed in almamap['seeds']:
-    #     input = int(seed)
-    #     for fromto in ['seed-to-soil', 'soil-to-fertilizer', 'fertilizer-to-water','water-to-light', 'light-to-temperature', 'temperature-to-humidity', 'humidity-to-location']:
-    #         output = mapfunc(almamap, fromto, input)
-    #         input = output
-    #     if closest:
-    #         closest = min(closest, output)
-    #     else:
-    #         closest = output
-    # print(closest)
+    locations = [almanac.seedtolocation(seed) for seed in almanac.seeds]
+    print(f'Shortest route from seed to location: %d' % min(locations))
